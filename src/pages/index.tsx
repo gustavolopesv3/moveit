14230079d97ebb479ff6aaaf7cc9ebec0c96196c
Gustavo Lopes <38574162+gustavolopesv3@users.noreply.github.com>
2021-02-23 22:23:32 -0500

    countdown

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,22 @@
 import { CompletedChallenges } from '../components/CompletedChallenges';
+import { Countdow } from '../components/Countdown';
 import { ExperienceBar } from '../components/ExperienceBar';
 import { Profile } from '../components/Profile';
 import styles from '../styles/pages/Home.module.css';
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Inicio | Move.it</title>
+      </Head>
       <ExperienceBar />
       <section>
         <div>
           <Profile />
           <CompletedChallenges />
+          <Countdow />
         </div>
         <div></div>
       </section>
